d48753ea36ea6043845b7d7f82b86dc3d79a153a
UlDev <uldev.communication@gmail.com>
2019-04-08 10:26:29 +0200

    Premier commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+?>
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Labyrinthe</title>
+    <link rel="icon" href="./files/logo.jpg" />
+    <link rel="stylesheet" href="./style/base.css">
+</head>
+    <body>
+        <h1>Bienvenue sur le projet Labyrinthe !</h1>
+        <p>Codez !!!!!!!!!!!!!!!!!</p>
+        <script type="text/javascript" src="./script/index.js"></script>
+    </body>
+</html>
