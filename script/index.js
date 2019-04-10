826905483e4520971e49598184afd65c68785c0a
ThomasSerre <thomas.serre@hotmail.fr>
2019-04-10 08:50:38 +0200

    inclusion plateau de depart

--- script/index.js
+++ script/index.js
@@ -1,49 +1,120 @@
+    let document_width = document.getElementById("GameContainer").clientWidth;
+    let document_height = document.getElementById("GameContainer").clientHeight;
+
+
     var config = {
         type: Phaser.AUTO,
-        width: document.getElementById("GameContainer").clientWidth,
-        height: document.getElementById("GameContainer").clientHeight,
+        width: document_width,
+        height: document_height,
         parent: "GameContainer",
         physics: {
             default: 'arcade',
-            arcade: {
-                gravity: { y: 200 }
-            }
         },
         scene: {
             preload: preload,
-            create: create
+            create: create,
+            update: update
         }
     };
 
+    let player;
+    let player2;
+    let blocks;
+    let cursors;
+
     var game = new Phaser.Game(config);
 
-    function preload ()
-    {
-        this.load.setBaseURL('http://labs.phaser.io');
+    function preload () {
+        this.load.image('background', 'files/fond.jpg');
+        this.load.image('caisse', 'files/block.png');
 
-        this.load.image('sky', 'assets/skies/space3.png');
-        this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-        this.load.image('red', 'assets/particles/red.png');
+
+        this.load.spritesheet('dude',
+            'files/dude.png',
+            { frameWidth: 32, frameHeight: 48 }
+        );
     }
 
-    function create ()
-    {
-        this.add.image(400, 300, 'sky');
+    function create () {
+        blocks = this.physics.add.staticGroup();
+        this.add.image(document_width / 2, document_height / 2, 'background');
+        let demi = document_width / 22;
+        for(let i = 1; i <=11; i +=2){
+            for(let j = 1; j<=11; j +=2){
+                blocks.create((document_width / 11)*i + demi, (document_height / 11)*j + demi, 'caisse');
+            }
+        }
 
-        var particles = this.add.particles('red');
+        player = this.physics.add.sprite(demi, demi, 'dude');
+        player.setCollideWorldBounds(true);
 
-        var emitter = particles.createEmitter({
-            speed: 100,
-            scale: { start: 1, end: 0 },
-            blendMode: 'ADD'
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
         });
 
-        var logo = this.physics.add.image(400, 100, 'logo');
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: 'dude', frame: 4 } ],
+            frameRate: 10
+        });
 
-        logo.setVelocity(100, 200);
-        logo.setBounce(1, 1);
-        logo.setCollideWorldBounds(true);
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
 
-        emitter.startFollow(logo);
+        this.anims.create({
+            key: 'down',
+            frames: [ { key: 'dude', frame: 4 } ],
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'up',
+            frames: [ { key: 'dude', frame: 4 } ],
+            frameRate: 10,
+            repeat: -1
+        });
+
+        cursors = this.input.keyboard.createCursorKeys();
+        this.physics.add.collider(player, blocks);
+    }
+    
+    function update() {
+        if (cursors.left.isDown){
+            player.setVelocityX(-160);
+            player.setVelocityY(0);
+
+            player.anims.play('left', true);
+        } else if (cursors.right.isDown) {
+            player.setVelocityX(160);
+            player.setVelocityY(0);
+
+            player.anims.play('right', true);
+        } else if(cursors.down.isDown) {
+            player.setVelocityX(0);
+            player.setVelocityY(160);
+
+            player.anims.play('down', true);
+        } else if(cursors.up.isDown) {
+            player.setVelocityX(0);
+            player.setVelocityY(-160);
+
+            player.anims.play('up', true);
+        } else {
+            player.setVelocityX(0);
+            player.setVelocityY(0);
+
+            player.anims.play('turn');
+        }
+        if (cursors.up.isDown && player.body.touching.down) {
+            player.setVelocityY(-330);
+        }
     }
 
